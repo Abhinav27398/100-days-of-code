865b27f1a189b3b48a5b07fed9af6fe099010aa3
Abhinav singh <Abhinav.27398@stu.upes.ac.in>
2025-10-01 22:21:53 +0530

    Add files via upload

---
+++ Q84.c
@@ -0,0 +1,27 @@
+/*
+Question:
+Write a C program to convert a lowercase string to uppercase without using built-in functions.
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    // Input string
+    printf("Enter a lowercase string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Convert to uppercase manually
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;  // Convert to uppercase
+        }
+    }
+
+    // Print result
+    printf("Uppercase string: %s\n", str);
+
+    return 0;
+}
